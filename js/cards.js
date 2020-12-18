06c62cf8dc269d927cbb758045e1a395aa47ca96
JoaoAugust <71889097+JoaoAugust@users.noreply.github.com>
2020-12-18 10:26:14 -0300

    Add files via upload

--- js/cards.js
+++ js/cards.js
@@ -55,6 +55,91 @@ function converter(event) {
     document.calc.visor.value = resultado;
     
  }
+ function VerificarTriangulo(){
+   let a = parseFloat(document.getElementById("numero1").value)
+   let b = parseFloat(document.getElementById("numero2").value)
+   let c = parseFloat(document.getElementById("numero3").value)
+   let resultado = ""
+
+   if (a == b && a == c && b == c) 
+   {
+       resultado = "O triangulo é Isóceles!";
+   }
+   else if(a != b && b != c && c != a)
+   {
+       resultado = "O triangulo é Escaleno!";
+   }
+   else{
+       resultado = "O triangulo é Equilátero!";
+   }
+   document.getElementById("result").textContent = resultado
+   
+}
+function VerificarNotas(){
+   let num1 = parseFloat(document.getElementById("num1").value)
+   let num2 = parseFloat(document.getElementById("num2").value)
+   let num3 = parseFloat(document.getElementById("num3").value)
+   let num4 = parseFloat(document.getElementById("num4").value)
+   let media_final = ""
+
+    media_final = (num1 + num2 + num3 + num4) / 4
+
+   if (media_final >= 5) 
+   {
+       resultado = "Você está Aprovado!";
+   }
+   else if(media_final < 5)
+   {
+       resultado = "Você está Reprovado!";
+   }
+   else{
+       resultado = "Você está de Recuperação!";
+   }
+   document.getElementById("media_final").textContent = resultado
+   
+}
+function conversaoMoeda(){
+   let resultado = ""
+   var opcao = document.getElementById("opções").value; 
+   let valor = document.getElementById("text2").value;
+   let dolarAmericano = 5.0;
+   let dolarCanadense = 4.0;
+   let dolarAustraliano = 3.0;
+   switch (opcao){
+       case "US$":
+           document.getElementById("valorFinal").textContent = (valor/dolarAmericano).toFixed(2);
+           break
+       case "C$":
+           document.getElementById("valorFinal").textContent = (valor/dolarCanadense).toFixed(2);
+           break
+       case "A$":
+           document.getElementById("valorFinal").textContent = (valor/dolarAustraliano).toFixed(2);
+           break
+       default:
+           document.getElementById("valorFinal").textContent = resultado
+   }
+   function verificarCPF(){
+      let cpf = document.getElementById("entrada").value
+      let resultado = ""
+  
+      //obter terceiro segmento do CPF 111.222.333-DD
+      let segmento = parseInt(cpf.split("-")[0].split(".")[2].split("")[2])
+  
+      switch(segmento)
+      {
+          case 1:
+              resultado = "SP"
+              break
+          case 4:
+              resultado = "PB"
+              break
+          case 9:
+              resultado = "RJ"
+              break
+      }
+      document.getElementById("saida").textContent = resultado
+  }
+}
 
 
 
