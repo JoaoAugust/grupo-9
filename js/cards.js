1dbe58c8fb2c4b498510aa27bf2b3b2b53c64e84
JoaoAugust <71889097+JoaoAugust@users.noreply.github.com>
2020-12-18 10:41:13 -0300

    Add files via upload

--- js/cards.js
+++ js/cards.js
@@ -119,28 +119,28 @@ function conversaoMoeda(){
            document.getElementById("valorFinal").textContent = resultado
    }
 }
-   function verificarCPF(){
-      let cpf = document.getElementById("entrada").value
-      let resultado = ""
-  
-      //obter terceiro segmento do CPF 111.222.333-DD
-      let segmento = parseInt(cpf.split("-")[0].split(".")[2].split("")[2])
-  
-      switch(segmento)
-      {
-          case 1:
-              resultado = "SP"
-              break
-          case 4:
-              resultado = "PB"
-              break
-          case 9:
-              resultado = "RJ"
-              break
-      }
-      document.getElementById("saida").textContent = resultado
-  }
+function verificarCPF(){
+   let cpf = document.getElementById("entrada").value
+   let resultado = ""
+
+   //obter terceiro segmento do CPF 111.222.333-DD
+   let segmento = parseInt(cpf.split("-")[0].split(".")[2].split("")[2])
+
+   switch(segmento)
+   {
+       case 1:
+           resultado = "SP"
+           break
+       case 4:
+           resultado = "PB"
+           break
+       case 9:
+           resultado = "RJ"
+           break
+   }
+   document.getElementById("saida").textContent = resultado
 }
+
 
 
 
